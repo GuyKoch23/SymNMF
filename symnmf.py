9df76e57e538533fa9af5db8556c5e65025a3ecb
GuyKoch <guykoch@mail.tau.ac.il>
2024-05-14 12:28:24 +0300

    Finished python

--- symnmf.py
+++ symnmf.py
@@ -1,21 +1,52 @@
 import sys
-import os
 import numpy as np
+import pandas as pd
 import math
 import symnmf
 
-def initialize_H():
-    pass
+def read_vectors(file_name):
+    data = pd.read_csv(file_name, header=None)
+    X = data.values.tolist()
+    return X
+
+def initialize_H(N, K, w_avg):
+    mat = [[np.random.uniform(0, 2*math.sqrt(w_avg/K)) for _ in range(K)] for _ in range(N)]
+    return mat
+
+def get_mat_avg(mat, rows, cols):
+    sum = 0
+    cnt = 0
+    for i in range(rows):
+        for j in range(cols):
+            sum += mat[i][j]
+            cnt+=1
+    return sum / cnt
+
+def print_result_matrix(mat, rows, cols):
+    for i in range(rows):
+        for j in range(cols):
+            print(mat[i][j])
 
 def main(K, goal, file_name): 
     np.random.seed(0)
+    X = read_vectors(file_name)
+    N = len(X)
+    d = len(X[0])
     if(goal == "symnmf"):
-        H = initialize_H()
+        W = symnmf.norm(X, N, d)
+        w_avg = get_mat_avg(W, N, N)
+        H = initialize_H(N, K, w_avg)
+        H = symnmf.symnmf(H, W, N, d)
+        print_result_matrix(H, N, K)
     elif(goal == "sym"):
-        N = 5
-        d = 3
-        A = symnmf.sym(goal, file_name, N, d)
-    
+        A = symnmf.sym(X, N, d)
+        print_result_matrix(A, N, N)
+    elif(goal == "ddg"):
+        D = symnmf.ddg(X, N, d)
+        print_result_matrix(D, N, N)
+    elif(goal == "norm"):
+        W = symnmf.norm(X, N, d)
+        print_result_matrix(W, N, N)
 
 if __name__ == "__main__":
     try:
