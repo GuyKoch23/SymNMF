b81fcfd95684d15e073d5fea65df4073502d8a67
GuyKoch <guykoch@mail.tau.ac.il>
2024-05-14 10:47:02 +0300

    Added header file

---
+++ symnmf.h
@@ -0,0 +1,7 @@
+double** sym(double** X, int N, int d);
+
+double** ddg(double** A, int N, int d);
+
+double** norm(double** A, double** D, int N, int d);
+
+double** symnmf(double** H, double** W, double eps, int max_iter, int N, int K);
